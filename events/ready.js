0b9a8b1c45766caaea0fc62296d2703bb6f3e3ae
Ganz <62270450+LeinadKenbun@users.noreply.github.com>
2021-09-17 13:58:03 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,8 @@
 module.exports = async (client) => {
-  client.Ready = true;
+  client.Ready = true, 
+  client.user.setPresence({
+    status: "online",  // You can show online, idle, and dnd
+});
     client.Manager.init(client.user.id);
     client.log("Successfully Logged in as " + client.user.tag); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
 client.RegisterSlashCommands();
